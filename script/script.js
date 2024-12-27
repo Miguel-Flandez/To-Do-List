4ad6999db9e1106bbb23b45d7e8dee65bfa0259f
[Miguel Flandez] <[immfpnvs2@gmail.com]>
2024-12-27 11:18:05 +0800

    here are the updates

--- script/script.js
+++ script/script.js
@@ -6,7 +6,8 @@ const todoTitleInput = document.getElementById("todoTitleInput");
 const submitButton = document.getElementById("submitButton");
 
 const popupModal2 = document.getElementById("popupModal2");
-const titleHolder = document.getElementById("titleHolder")
+const titleHolder = document.getElementById("titleHolder");
+const noteHolder = document.getElementById("noteHolder");
 
 
 window.addEventListener("keydown",function(event){
@@ -44,44 +45,94 @@ todoTitleInput.addEventListener('keydown', function(event){
 const arr = [];
 
 function addList (title){
-    const newTodo = document.createElement("div");
+const newTodo = document.createElement("div");
+
+
+arr.push({
+    title:title,
+    "to-do": [{
+        chk: false,
+        note: ""
+    }]
+})
+
+const newIndex = arr.length - 1;
+
+newTodo.classList.add('to-do-lists');
+newTodo.innerHTML = `<p>${title}</p>`;
+newTodo.setAttribute('data-id',newIndex);
+
+listContainer.appendChild(newTodo);
+
+newTodo.addEventListener('click',function(){
+    const noteTitle = document.createElement('div');
     
+    const index = newTodo.getAttribute('data-id');
+    const todoData = arr[index];
 
-    arr.push({
-        title:title,
-        "to-do": {}
-    })
+    titleHolder.innerHTML = '';
+    noteTitle.innerHTML = `<h3>${todoData.title}</h3>`;  
+    noteTitle.id = "noteTitle";
 
-    const newIndex = arr.length - 1;
+    noteHolder.innerHTML = '';
 
+    const saveNote = document.getElementById('saveNote');
 
-    newTodo.classList.add('to-do-lists');
-    newTodo.innerHTML = `<p>${title}</p>`;
-    newTodo.setAttribute('data-id',newIndex);
+    todoData['to-do'].forEach(element => {
+        const checkBox = document.createElement('input');
+        const note = document.createElement('input');
 
-    listContainer.appendChild(newTodo);
+        checkBox.id = 'checkBox';
+        checkBox.type = 'checkbox';
+        checkBox.checked = element.chk;
 
-    newTodo.addEventListener('click',function(){
-        const noteTitle = document.createElement('div');
-        const index = newTodo.getAttribute('data-id');
-        const todoData = arr[index];
+        note.type = 'text';
+        note.id = 'note';
+        note.value = element.note;
 
-        titleHolder.innerHTML = '';
+        noteHolder.appendChild(checkBox);
+        noteHolder.appendChild(note);
 
-         noteTitle.classList.add("noteTitle");
-         noteTitle.innerHTML = `<h3>${todoData.title}</h3>`;    
-         popupModal2.style.display = "flex";
+        saveNote.addEventListener('click',function(){
+            element.note = note.value;
+            element.chk = checkBox.checked;
+            popupModal2.style.display = "none";
+        })
+    });
+    
+    popupModal2.style.display = "flex";
 
-         titleHolder.appendChild(noteTitle);
+    titleHolder.appendChild(noteTitle);
 
-         return todoData;
-    })
+    if(popupModal2.style.display==='flex'){
+        window.addEventListener('keydown',function(e){
+            if(e.key==='Enter'){
+                const checkBox = document.createElement('input');
+                const note = document.createElement('input');
+
+                checkBox.id = 'checkBox';
+                checkBox.type = 'checkbox';
+                checkBox.checked = false;
+
+                note.type = 'text';
+                note.id = 'note';
+                note.value = '';
+
+                noteHolder.appendChild(checkBox);
+                noteHolder.appendChild(note);
+
+                todoData['to-do'].push({
+                    chk: checkBox.checked,
+                    note: note.value
+                })
+            }
+        })
+    }
+})
 }
-
 popupModal.addEventListener('click', function(e) {
     if (e.target === popupModal) {
         popupModal.style.display = "none"; // Close modal when clicking outside
-
     }
 })
 window.addEventListener('keydown', function(e) {
